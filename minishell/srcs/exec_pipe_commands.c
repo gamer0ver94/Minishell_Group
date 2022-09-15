2d69a2738a8ae8f1939452b3f42aec1d6944cba9
gamer0ver94 <duarte.paulino94@hotmail.com>
2022-09-15 17:00:44 +0200

    ...

--- minishell/srcs/exec_pipe_commands.c
+++ minishell/srcs/exec_pipe_commands.c
@@ -17,10 +17,52 @@ int	count_pipes(t_command **prompt)
 
 void exec_pipe_commands(t_command **prompt, char **envp)
 {
-	int fd;
+	(void)prompt;
+	(void)envp;
+	int fd[2];
+	pid_t pid;
+	pid_t pid2;
+	t_command *tmp;
 
-	fd = open("text.txt");
-	write(fd,"fsafsafsa", 6)
+	tmp = (*prompt);
+	if(pipe(fd) == -1)
+		exit(0);
+	pid = fork();
+	if (pid == -1)
+		exit(0);
+	if (pid == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		exec_command(tmp, envp);
+	}
+	else
+	{
 
+	}
+		pid2 = fork();
+		tmp = tmp->next;
+		if (pid2 == -1)
+			exit(0);
+		if (pid2 == 0)
+		{
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[1]);
+			close(fd[0]);
+			exec_command(tmp, envp);
+		}
+		else
+		{
+			tmp = tmp->next;
+			if(waitpid(pid, NULL, 0) == -1)
+				printf("waiting problem\n");
+			if(waitpid(pid2, NULL, 0) == -1)
+				printf("waiting problem\n");
+			close(fd[0]);
+			close(fd[1]);
+		}
+		
 
+	
 }
