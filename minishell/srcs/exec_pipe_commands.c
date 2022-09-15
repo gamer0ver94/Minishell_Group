aae8517d781117ade9e12a7ac357258b017e3150
gamer0ver94 <duarte.paulino94@hotmail.com>
2022-09-15 11:51:46 +0200

    test

--- minishell/srcs/exec_pipe_commands.c
+++ minishell/srcs/exec_pipe_commands.c
@@ -17,52 +17,10 @@ int	count_pipes(t_command **prompt)
 
 void exec_pipe_commands(t_command **prompt, char **envp)
 {
-	int fd[2];
-	int *pid;
-	t_command *tmp;
-	int	i;
+	int fd;
 
-	i = 0;
-	pid = malloc(sizeof(int) * count_pipes(prompt));
-	tmp = (*prompt);
-	if(pipe(fd) == -1)
-		printf("pipe error\n");
-	pid[i] = fork();
-	if (pid[i] == -1)
-		printf("ups no fork here");
-	if (pid[i] == 0)
-	{
-		dup2(fd[1],STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		exec_command(tmp, envp);
-	}
-	tmp = tmp->next;
-	i++;
-	while(tmp)
-	{
-		// tmp = tmp->next;
-		// i++;
-		pid[i] = fork();
-		printf("%d\n", i);
-		if (pid[i] == -1)
-			printf("ups no fork here");
-		if (pid[i] == 0)
-		{
-			dup2(fd[0],STDIN_FILENO);
-			close(fd[0]);
-			close(fd[1]);
-			exec_command(tmp,envp);
-		}
-		else
-		{
-			tmp = tmp->next;
-			waitpid(pid[i],NULL, 0);
-		}
-	}
-	i = 0;
-	close(fd[0]);
-	close(fd[1]);
-	free(pid);
+	fd = open("text.txt");
+	write(fd,"fsafsafsa", 6)
+
 
 }
