746cf5ed19427540122900a53ba3fab3986fe5f4
Mahmoud Emam <mahmoudemam@MacBook-Pro-de-Mahmoud.local>
2022-09-23 00:17:50 +0200

    not finshe

--- minishell/srcs/ft_unset.c
+++ minishell/srcs/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: memam <memam@student.42mulhouse.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 18:15:50 by memam             #+#    #+#             */
-/*   Updated: 2022/09/20 19:51:25 by memam            ###   ########.fr       */
+/*   Updated: 2022/09/23 00:08:35 by memam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,13 @@ int	ft_unset(char **envp, char **args)
 		index = get_env_var_index(envp, args[i]);
 		if (index == -1)
 			printf("unset: %s: variable not found\n", args[i]);
-		for (int v = 0; envp[v]; v++)
-		{
-			printf("[%d]%s\n", v, envp[v]);
-		}
+		
 		printf("is ok %s\n", envp[index]);
 		while (envp[index])
 		{
 			envp[index] = ft_strdup(envp[index + 1]);
 			free(envp[index + 1]);
 		}
-		envp = realloc_env_var(envp, index);
 	}
 	return (0);
 }
