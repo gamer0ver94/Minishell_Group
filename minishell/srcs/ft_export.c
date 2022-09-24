64f30f6f8978b66cb81a13de07460506e1b19a16
Mahmoud <mahmoudeladly@hotmail.fr>
2022-09-24 18:45:15 +0200

    fini

--- minishell/srcs/ft_export.c
+++ minishell/srcs/ft_export.c
@@ -6,19 +6,13 @@
 /*   By: memam <memam@student.42mulhouse.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 12:02:06 by memam             #+#    #+#             */
-/*   Updated: 2022/09/23 16:35:14 by memam            ###   ########.fr       */
+/*   Updated: 2022/09/24 15:22:26 by memam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-/*  
-*       check if the identifier is valid for evrionnment variable.
-*       return true if the identifier contains only alphanumeric or '_'
-*       or false if not.
-*/
-
-int	is_valid_env_var_idVar(char *var)
+int	is_valid_env_var(char *var)
 {
 	int	i;
 
@@ -34,34 +28,24 @@ int	is_valid_env_var_idVar(char *var)
 	}
 	return (1);
 }
-/* 
-*       add the given variable to the environment variable.
-*       return 0 if all args added to env.
-*       return 1 if one or more args not added to env. 
-*/
 
 int	ft_export(char **envp, char **args)
 {
 	int		i;
-	int		e;
-	//char	**tmp;
 
 	i = 1;
-	e = 0;
 	if (!args[i])
 		return (ft_env(envp));
 	while (args[i])
 	{
-		if (!is_valid_env_var_idVar(args[i]))
+		if (!is_valid_env_var(args[i]))
 		{
 			printf("export: %s not a valid identifier\n", args[i]);
 			return (1);
 		}
 		else if (args[i] != NULL)
-		{	printf("form export\n");
-
+		{
 			set_env_var(envp, args[i]);
-			break ;
 		}
 		i++;
 	}
