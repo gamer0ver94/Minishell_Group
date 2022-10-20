9abdd86502d19bb2ed8d2cf5979ca4fc0b9cb69f
Mahmoud Emam <mahmoudemam@MacBook-Pro-de-Mahmoud.local>
2022-10-20 23:51:02 +0200

    add the new function

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpaulino <dpaulino@student.42.fr>          +#+  +:+       +#+        */
+/*   By: memam <memam@student.42mulhouse.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 13:25:13 by dpaulino          #+#    #+#             */
-/*   Updated: 2022/10/18 13:52:30 by dpaulino         ###   ########.fr       */
+/*   Updated: 2022/10/20 23:35:39 by memam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,5 +147,6 @@ t_execc *exe);
 void		ft_close_exit_complex(t_command **prompt, t_execc *exe, int *file, \
 char **envp);
 void		export_list(char **envp);
+void        get_signals(int sig);
 
 #endif
