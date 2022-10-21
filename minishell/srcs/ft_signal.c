7ae929f367daed0bdeda134ed17ec4aa1e8bf71a
Mahmoud <mahmoudeladly@hotmail.fr>
2022-10-21 13:50:09 +0200

    new function signal

--- minishell/srcs/ft_signal.c
+++ minishell/srcs/ft_signal.c
@@ -6,52 +6,53 @@
 /*   By: memam <memam@student.42mulhouse.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 22:32:59 by memam             #+#    #+#             */
-/*   Updated: 2022/10/20 23:37:43 by memam            ###   ########.fr       */
+/*   Updated: 2022/10/21 13:23:43 by memam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void   sighandler(int sig)
+void	sighandler(int sig)
 {
-    g_status = 130;
- 	if (sig == SIGQUIT)
- 	{
- 		printf("\nThank you for using MINISHELL\n");
- 		exit(0);
+	g_status = 130;
+	if (sig == SIGQUIT)
+	{
+		printf("\nThank you for using MINISHELL\n");
+		exit(0);
 	}
-    else if (sig == SIGINT)
-    {
-        write(1, "\n", 1);
-        //rl_replace_line("", 0);
-        rl_on_new_line();
-        rl_redisplay();
-    }
+	else if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
-void    ctrl_c(int sig)
+void	ctrl_c(int sig)
 {
-    g_status = 130;
-    write(1, "\n", 1);
-    (void)sig;
+	g_status = 130;
+	write(1, "\n", 1);
+	(void)sig;
 }
 
-void back_slash(int sig)
+void	back_slash(int sig)
 {
-    g_status = 131;
-    printf("Quit....\n");
-    (void)sig;
+	g_status = 131;
+	printf("Quit: 3\n");
+	(void)sig;
 }
-void    get_signals(int sig)
+
+void	get_signals(int sig)
 {
-    if (sig == 1)
-    {
-        signal(SIGINT, sighandler);
-        signal(SIGQUIT, SIG_IGN);
-    }
-    if (sig == 2)
-    {
-        signal(SIGINT, ctrl_c);
-        signal(SIGQUIT, back_slash);
-    }
+	if (sig == 1)
+	{
+		signal(SIGINT, sighandler);
+		signal(SIGQUIT, SIG_IGN);
+	}
+	if (sig == 2)
+	{
+		signal(SIGINT, ctrl_c);
+		signal(SIGQUIT, back_slash);
+	}
 }
